ee5d0fc755b23aa85bbb16bc31b146f7cbb29636
jacob zlogar <jacob.zlogar@gmail.com>
2016-10-28 23:05:17 -0400

    vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,7 +13,7 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.component('home', require('./components/Home.vue'));
 
 const app = new Vue({
     el: '#app'
--- resources/assets/js/components/Home.vue
+++ resources/assets/js/components/Home.vue
@@ -3,10 +3,10 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
+                    <div class="panel-heading">Logged In</div>
 
                     <div class="panel-body">
-                        I'm an example component!
+                        You have logged in.
                     </div>
                 </div>
             </div>
